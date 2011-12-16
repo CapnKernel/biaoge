4b480449ffd34649c8fcd9669b04650a3e9345a8
Mitch Davis <mjd@afork.com>
2011-12-16 14:17:13 +0800

    We don't need to make the args into ints, as they're fed to %s.

--- biaoge.py
+++ biaoge.py
@@ -5,7 +5,7 @@ import csv
 print open("biaoge-ps-header.txt").read()
 
 for field in csv.DictReader(open("dhl.csv")):
-  print "newpath (%s) %s %s %s text closepath stroke" % (field['Value'], int(field['X']), field['Y'], int(field['Font']))
+  print "newpath (%s) %s %s %s text closepath stroke" % (field['Value'], field['X'], field['Y'], field['Font'])
 
 print open("biaoge-ps-trailer.txt").read()
 
